76e97b90d7e4ae870d2a622d40ed722cd3520995
Eric Malloy <malloy.j.eric@gmail.com>
2016-10-19 16:08:19 -0400

    launch the template, output the secret access keypair

--- cf_launcher.sh
+++ cf_launcher.sh
@@ -2,56 +2,62 @@
 set -o errexit
 set -x
 
-if ! hash aws >/dev/null 2>&1; then
-  printf "Can't find awscli, exiting ...."
-  exit 1
-fi
+
+for y in aws jq; do
+  if ! hash $y >/dev/null 2>&1; then
+    printf "Can't find $y, exiting ...."
+    exit 1
+  fi
+done
 
 usage() {
-echo "Script takes seven parameters: \
-$0 <db_type> <stackName> <keyPairName> <vpcId> <subnetId> <amiId> <securityGroupId>"
+echo "Script takes five parameters: \
+$0 <region> <user> <bucket> <project> <ticket>"
 }
 
-if [[ "$#" -lt 6 ]]; then
+if [[ "$#" -lt 3 ]]; then
   usage && exit 1
 fi
 
-if [[ $1 == "cassandra" ]]; then
-    DB_TYPE="cassandra"
-    elif [[ $1 = "mongod" ]]; then
-    DB_TYPE="mongod";
-    elif [[ $1 = "nfs" ]]; then
-    DB_TYPE="nfs";
-else
-    printf "db_type must be cassandra or mongod or nfs\n"
-    exit 1
-fi
-
 # eventually all these will be replaced with lookups
-STACK_NAME=$2
-KEY_NAME=$3
-VPC_ID=$4
-SUBNET_ID=$5
-AMI_ID=$6
-SECURITY_GROUP_ID=$7
-REGION=$8
-INSTANCE_TYPE="m4.xlarge"
+export REGION=$1
+export USER=$2
+export BUCKET=$3
+export PROJECT=$4
+export TICKET=$5
 
-aws cloudformation create-stack \
-  --stack-name ${STACK_NAME} \
-  --on-failure DO_NOTHING \
-  --template-body file:////$(pwd)/${DB_TYPE}.template \
-  --parameters ParameterKey=KeyName,ParameterValue=${KEY_NAME}\
+
+aws --region ${REGION} \
+  cloudformation create-stack \
+  --capabilities CAPABILITY_IAM \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --stack-name ${REGION}-${PROJECT}-${TICKET} \
+  --timeout-in-minutes 3 \
+  --template-body file:////$(pwd)/cf-templates/iamUser.json \
+  --parameters ParameterKey=UserName,ParameterValue=${USER}\
 ,UsePreviousValue=False \
-ParameterKey=VpcId,ParameterValue=${VPC_ID}\
+ParameterKey=BucketName,ParameterValue=${BUCKET}\
 ,UsePreviousValue=False \
-ParameterKey=SubnetId,ParameterValue=${SUBNET_ID}\
+ParameterKey=ProjectName,ParameterValue=${PROJECT}\
 ,UsePreviousValue=False \
-ParameterKey=AmiId,ParameterValue=${AMI_ID}\
-,UsePreviousValue=False \
-ParameterKey=SecurityGroupId,ParameterValue=${SECURITY_GROUP_ID}\
-,UsePreviousValue=False \
-ParameterKey=InstanceType,ParameterValue=${INSTANCE_TYPE}\
-,UsePreviousValue=False \
-ParameterKey=Region,ParameterValue=${REGION}\
-,UsePreviousValue=False
+ParameterKey=TicketNumber,ParameterValue=${TICKET} \
+ | tee  ${TICKET}.txt
+
+export STACK_NAME=$(cat ${TICKET}.txt | jq -r .StackId)
+
+# going to be lazy here and just sleep rather than writting a check/wait/check func
+
+
+docfnquery() {
+aws --region ${REGION} \
+ cloudformation describe-stacks \
+ --stack-name ${STACK_NAME} | tee /tmp/${TICKET}.txt
+}
+
+inspectquery() {
+jq -e -M '.Stacks[].Outputs[].OutputValue' /tmp/${TICKET}.txt
+}
+
+retcode=5; while [ ${retcode} -ne 0 ]; do
+    sleep 15 && docfnquery && inspectquery && retcode=$?
+    done
